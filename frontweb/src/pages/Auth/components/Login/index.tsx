63282134b79b848446dbcf18a3670745d324174d
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-07-27 08:39:35 -0300

    Manual error handling

--- frontweb/src/pages/Auth/components/Login/index.tsx
+++ frontweb/src/pages/Auth/components/Login/index.tsx
@@ -1,15 +1,43 @@
+import { useForm } from 'react-hook-form';
 
+import { requestBackendLogin } from '../../../../util/requests';
 import ButtonPrimary from '../../../../components/ButtonPrimary';
 import './styles.css';
+import { useState } from 'react';
+
+type FormData = {
+	username: string;
+	password: string;
+};
 
 const Login = () => {
+	const [hasError, setHasError] = useState(false);
+	const { register, handleSubmit } = useForm<FormData>();
+	const onSubmit = (formData: FormData) => {
+		requestBackendLogin(formData)
+			.then((response) => {
+				setHasError(false);
+				console.log('SUCESSO ', response);
+			})
+			.catch((error) => {
+				setHasError(true);
+				console.log('ERRO ', error);
+			});
+	};
+
 	return (
-    <div className="login-card base-card">
+		<div className="login-card base-card">
 			<h1 className="login-card-title">Login</h1>
-			
-			<form>
+
+			{hasError && (
+				<div className="alert alert-danger">
+					Usuário ou senha inválidos
+				</div>
+			)}
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className="mb-4">
 					<input
+						{...register('username')}
 						type="text"
 						className={`form-control base-input `}
 						placeholder="Email"
@@ -19,7 +47,8 @@ const Login = () => {
 				</div>
 				<div className="mb-4">
 					<input
-						type="text"
+						{...register('password')}
+						type="password"
 						className={`form-control base-input `}
 						placeholder="Senha"
 						name="password"
@@ -30,7 +59,6 @@ const Login = () => {
 					<ButtonPrimary text="Fazer login" />
 				</div>
 			</form>
-
 		</div>
 	);
 };
