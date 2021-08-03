053a5d5c86fa81ad121acdfe60166170df6a0a79
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-08-02 23:11:47 -0300

    Layout x-sm catalog finished

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,19 +1,23 @@
-import { Link } from 'react-router-dom';
+import { ReactComponent as MovieImage } from '../../assets/images/movie.svg';
 import './styles.css';
 
 const Catalog = () => {
 	return (
 		<div className="catalog-container">
-			<div className="catalog-title">
-				<h1>Tela listagem de filmes</h1>
+			<div className="base-card catalog-filter-container">
+				Filtro Gêrnero
 			</div>
 			<div className="catalog-body">
-				<Link to="movies/1">
-					<span>Acessar /movies/1</span>
-				</Link>
-				<Link to="movies/2">
-					<span>Acessar /movies/2</span>
-				</Link>
+				<div className="base-card catalog-movie-card">
+					<div className="catalog-movie-image">
+						<MovieImage />
+					</div>
+					<div className="catalog-movie-description">
+						<h3>O Retorno do Rei</h3>
+						<h4>2013</h4>
+						<p>O olho do inimigo está se movendo.</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
