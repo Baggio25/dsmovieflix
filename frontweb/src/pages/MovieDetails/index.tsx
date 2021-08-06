9c4407941f08de0c4ee1d081ad49e59f1db1a8f9
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-08-06 16:34:41 -0300

    MovieDetails loader, fix loading movie and fix post/get reviews

--- frontweb/src/pages/MovieDetails/index.tsx
+++ frontweb/src/pages/MovieDetails/index.tsx
@@ -1,15 +1,17 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import { Movie } from '../../types/movie';
 import { ReviewInsert } from '../../types/review';
 import { hasAnyRoles } from '../../util/auth';
-import history from '../../util/history';
 import { requestBackend } from '../../util/requests';
 import ReviewList from './components/ReviewList';
 import './styles.css';
+import CardLoader from './../Catalog/CardLoader/index';
+import MovieDetailsLoader from './MovieDetailsLoader';
 
 type ParamsType = {
 	movieId: string;
@@ -18,12 +20,19 @@ type ParamsType = {
 const MovieDetails = () => {
 	const { movieId } = useParams<ParamsType>();
 	const [movie, setMovie] = useState<Movie>();
+	const [movieReview, setMovieReview] = useState<Movie>();
+	const [isLoading, setIsLoading] = useState(false);
 
 	const {
+		setValue,
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm<ReviewInsert>();
+
+	const handleFormClear = () => {
+		setValue('text', '');
+	};
 
 	const onSubmit = (formData: ReviewInsert) => {
 		const data = {
@@ -40,79 +49,112 @@ const MovieDetails = () => {
 
 		requestBackend(configReview)
 			.then(() => {
-				history.go(0);
-				console.log('Gravou', configReview.data);
+				getMovieReview();
+				handleFormClear();
+				toast.success('Avaliação cadastrada com sucesso');
 			})
 			.catch((error) => {
-				alert('Erro ao tentar adicionar Review ' + error);
+				if (error.response.status === 422) {
+					toast.error('Não é permitido gravar apenas espaços');
+				} else {
+					toast.error('Erro ao cadastrar avaliação');
+				}
 			});
 	};
 
-	useEffect(() => {
+	const getMovie = useCallback(() => {
 		const config: AxiosRequestConfig = {
 			method: 'GET',
 			url: `/movies/${movieId}`,
 			withCredentials: true,
 		};
 
+		setIsLoading(true);
+		requestBackend(config)
+			.then((response) => {
+				setMovie(response.data);
+			})
+			.finally(() => setIsLoading(false));
+	}, [movieId]);
+
+	const getMovieReview = useCallback(() => {
+		const config: AxiosRequestConfig = {
+			method: 'GET',
+			url: `/movies/${movieId}/reviews`,
+			withCredentials: true,
+		};
+
 		requestBackend(config).then((response) => {
-			setMovie(response.data);
-			console.log(response.data);
+			setMovieReview(response.data);
 		});
 	}, [movieId]);
 
+	useEffect(() => {
+		getMovie();
+	}, [getMovie]);
+
+	useEffect(() => {
+		getMovieReview();
+	}, [getMovieReview]);
+
 	return (
-		<div className="movie-details-container">
-			<div className="base-card movie-details">
-				<div className="movie-details-image">
-					<img src={movie?.imgUrl} alt={movie?.title} />
-				</div>
-				<div className="movie-details-description-container">
-					<div className="movie-details-description">
-						<h3>{movie?.title}</h3>
-						<h4>{movie?.year}</h4>
-						<p>{movie?.subTitle}</p>
-					</div>
-					<div className="movie-details-synopse">
-						<p>{movie?.synopsis}</p>
-					</div>
-				</div>
-			</div>
-			{hasAnyRoles(['ROLE_MEMBER']) && (
-				<div className="movie-details-card-new-review base-card">
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="mb-4">
-							<input
-								{...register('text', {
-									required: 'Campo obrigatório',
-								})}
-								type="text"
-								className={`form-control base-input ${
-									errors.text ? 'is-invalid' : ''
-								}`}
-								placeholder="Deixe sua avaliação aqui"
-								name="text"
-							/>
-						</div>
-						<div className="invalid-feedback d-block">
-							{errors.text?.message}
-						</div>
-						<div className="movie-details-card-submit">
-							<ButtonPrimary text="SALVAR AVALIAÇÃO" />
-						</div>
-					</form>
-				</div>
-			)}
-			{movie?.reviews.length === 0 ? (
-				<></>
+		<>
+			{isLoading ? (
+				<MovieDetailsLoader />
 			) : (
-				<div className="movie-details-card-reviews base-card">
-					{movie?.reviews.map((review) => (
-						<ReviewList key={review.id} review={review} />
-					))}
+				<div className="movie-details-container">
+					<div className="base-card movie-details">
+						<div className="movie-details-image">
+							<img src={movie?.imgUrl} alt={movie?.title} />
+						</div>
+						<div className="movie-details-description-container">
+							<div className="movie-details-description">
+								<h3>{movie?.title}</h3>
+								<h4>{movie?.year}</h4>
+								<p>{movie?.subTitle}</p>
+							</div>
+							<div className="movie-details-synopse">
+								<p>{movie?.synopsis}</p>
+							</div>
+						</div>
+					</div>
+					{hasAnyRoles(['ROLE_MEMBER']) && (
+						<div className="movie-details-card-new-review base-card">
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<div className="mb-4">
+									<input
+										{...register('text', {
+											required: 'Campo obrigatório',
+										})}
+										type="text"
+										className={`form-control base-input ${
+											errors.text ? 'is-invalid' : ''
+										}`}
+										placeholder="Deixe sua avaliação aqui"
+										name="text"
+									/>
+								</div>
+								<div className="is-invalid-default d-block">
+									{errors.text?.message}
+								</div>
+								<div className="movie-details-card-submit">
+									<ButtonPrimary text="SALVAR AVALIAÇÃO" />
+								</div>
+							</form>
+						</div>
+					)}
+					{movieReview?.reviews.length === 0 ? (
+						<></>
+					) : (
+						<div className="movie-details-card-reviews base-card">
+							{movieReview?.reviews.map((review) => (
+								<ReviewList key={review.id} review={review} />
+							))}
+						</div>
+					)}
 				</div>
 			)}
-		</div>
+		</>
 	);
 };
 
